dbb12e8ca881d2e25a2f13d609014915ad411452
athul rajesh <athul.rjsh@gmail.com>
2020-09-20 15:53:19 +0530

    initial

---
+++ src/init.js
@@ -0,0 +1,23 @@
+import game, { handleUserAction } from "./gameState";
+import initButtons from "./buttons";
+import { TICK_RATE } from "./constants";
+
+async function init() {
+  initButtons(handleUserAction);
+
+  console.log("starting game");
+
+  let nextTimeToTick = Date.now();
+  function nextAnimationFrame() {
+    const now = Date.now();
+    if (nextTimeToTick <= now) {
+      game.tick();
+      nextTimeToTick = now + TICK_RATE;
+    }
+    requestAnimationFrame(nextAnimationFrame);
+  }
+
+  nextAnimationFrame();
+}
+
+init();
